6420ac484df40b6050667fb095ffcf289bd31b4b
Avijeet pandey <avijeetpandey25@gmail.com>
2024-01-28 18:21:41 +0530

    Add: room db cleanup

--- app/src/main/java/com/example/agenda/activities/MainActivity.kt
+++ app/src/main/java/com/example/agenda/activities/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.agenda.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.agenda.R
 import com.example.agenda.adapters.AgendaAdapter
+import com.example.agenda.entities.AgendaItem
+import com.example.agenda.roomdb.AgendaDatabase
 import com.example.agenda.viewModels.AgendaViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         val adapter = AgendaAdapter(dataSet)
-
+        
         recyclerView?.adapter = adapter
     }
 }
