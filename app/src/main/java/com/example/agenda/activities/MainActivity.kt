c27867ee81f542320f2055ba808ee54d80ca0222
Avijeet pandey <avijeetpandey25@gmail.com>
2024-01-28 16:48:27 +0530

    Add: change to populate data from the viewmodel

--- app/src/main/java/com/example/agenda/activities/MainActivity.kt
+++ app/src/main/java/com/example/agenda/activities/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.agenda.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.agenda.R
+import com.example.agenda.adapters.AgendaAdapter
+import com.example.agenda.viewModels.AgendaViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private var recyclerView: RecyclerView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView?.layoutManager = LinearLayoutManager(this)
+
+        var dataSet = ArrayList<AgendaViewModel>()
+
+        for(i in 1..10) {
+            val agendaViewModel = AgendaViewModel(title = "Dinner",
+                                                subtitle = "Please have dinner on time",
+                                                isCompleted = false)
+            dataSet.add(agendaViewModel)
+        }
+
+        val adapter = AgendaAdapter(dataSet)
+
+        recyclerView?.adapter = adapter
     }
 }
