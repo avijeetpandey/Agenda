9a83993f168b8ef0fcb9ea23763d9eea16a32653
Avijeet pandey <avijeetpandey25@gmail.com>
2024-01-28 18:40:42 +0530

    Add: Functionality to fetch data and render on screen

--- app/src/main/java/com/example/agenda/activities/MainActivity.kt
+++ app/src/main/java/com/example/agenda/activities/MainActivity.kt
@@ -6,30 +6,54 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.agenda.R
 import com.example.agenda.adapters.AgendaAdapter
+import com.example.agenda.roomdb.AgendaDatabase
 import com.example.agenda.viewModels.AgendaViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private var recyclerView: RecyclerView? = null
+    private var dataSet = ArrayList<AgendaViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView = findViewById(R.id.recyclerView)
         recyclerView?.layoutManager = LinearLayoutManager(this)
-
-        var dataSet = ArrayList<AgendaViewModel>()
-
-        for(i in 1..10) {
-            val agendaViewModel = AgendaViewModel(title = "Dinner",
-                                                subtitle = "Please have dinner on time",
-                                                isCompleted = false)
-            dataSet.add(agendaViewModel)
-        }
-
-        val adapter = AgendaAdapter(dataSet)
         
+        val adapter = AgendaAdapter(dataSet)
         recyclerView?.adapter = adapter
+
+        fetchDataFromTheRoomDB()
+    }
+
+    private fun fetchDataFromTheRoomDB() {
+        GlobalScope.launch {
+            withContext(Dispatchers.Main) {
+                this@MainActivity?.let {
+                    if(AgendaDatabase(it).AgendaItemDao().getAllAgendaItems().isNotEmpty()) {
+                        val agendaFromDB = AgendaDatabase(it).AgendaItemDao().getAllAgendaItems()
+                        
+                        agendaFromDB.forEach {
+                            val agendaViewModel = AgendaViewModel(it.title,
+                                                                it.subtitle,
+                                                                it.isCompleted)
+
+                            dataSet.add(agendaViewModel)
+                        }
+
+                        recyclerView?.adapter?.notifyDataSetChanged()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun insertDataIntoDB() {
+
     }
 }
