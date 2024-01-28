b6e78c5e2d5ae3a6aafad4db3bfd3e987ab24369
Avijeet pandey <avijeetpandey25@gmail.com>
2024-01-28 15:54:54 +0530

    Add: Splash screen

--- app/src/main/java/com/example/agenda/MainActivity.kt
+++ app/src/main/java/com/example/agenda/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.agenda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
